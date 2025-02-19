96557553bbf086c846cdff27cba48a2655e362c7
moon-2013 <moon.company.2013@gmail.com>
2025-02-20 01:31:02 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,14 @@
-     
-            
-import os         
-         
+  
+
+import os
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///moon.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:////home/moon2013/moon/instance/moon.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "your_jwt_secret_key"
-   
-   
-      
-      
-      
-      
+
+
+
+
    
