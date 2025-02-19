46056cdd606c8b9b58f5559842101615f6cca069
moon-2013 <moon.company.2013@gmail.com>
2025-02-19 16:21:02 +0300

    Delete moon directory

--- moon/app/models.py
+++
@@ -1,9 +0,0 @@
-          
-from app.extensions import db    
-        
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    
--- moon/app/routes/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, request
-
-home_bp = Blueprint("home", __name__)
-
-@home_bp.route("/", methods=["GET"])
-def home():
-    """API الصفحة الرئيسية التي تعرض بيانات مختلفة حسب الموقع الذي يطلبها"""
-                    
-    return jsonify({"message": "00000001111111111111"})
