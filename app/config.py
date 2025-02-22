c039219357505de7377b383e09214937b249c889
moon-2013 <moon.company.2013@gmail.com>
2025-02-22 23:50:28 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
     
-    import os
+import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # المسار الرئيسي للمشروع
 DB_PATH = os.path.join(BASE_DIR, "app", "moon.db")  # وضع قاعدة البيانات داخل app
