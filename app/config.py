919f8a4af6311a20b2db19df4662ce566357ec41
moon-2013 <moon.company.2013@gmail.com>
2025-02-22 23:12:57 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,17 +1,13 @@
     
-    
- 
-import os
+    import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # المسار الأساسي للمشروع
-DB_PATH = os.path.join(BASE_DIR, "app", "moon.db")  # وضع قاعدة البيانات في مجلد app
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # المسار الرئيسي للمشروع
+DB_PATH = os.path.join(BASE_DIR, "app", "moon.db")  # وضع قاعدة البيانات داخل app
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "your_jwt_secret_key"    
-  
-  
+    JWT_SECRET_KEY = "your_jwt_secret_key"
   
     
