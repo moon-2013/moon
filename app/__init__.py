e5eb80accee61b4e2d07c16f5d25c918b0d6eb92
moon-2013 <moon.company.2013@gmail.com>
2025-03-04 22:02:42 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,3 @@
-                
-
- 
-  
 import os
 import time
 from flask import Flask, request
@@ -9,8 +5,9 @@ from app.config import Config
 from app.extensions import db, jwt
 from app.routes.auth import auth_bp
 from app.routes.users import users_bp
-from flask_cors import CORS
 from app.routes.home import home_bp
+from app.routes.posts import posts_bp  # إضافة مسار المنشورات
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
@@ -18,7 +15,8 @@ def create_app():
     # ✅ تحميل الإعدادات من Config
     app.config.from_object(Config)
 
-    # ✅ تفعيل CORS
+    # ✅ تفعيل CORS 
+
     CORS(app, resources={r"/*": {"origins": "*"}})
 
     # ✅ تهيئة الإضافات
@@ -26,9 +24,10 @@ def create_app():
     jwt.init_app(app)
 
     # ✅ تسجيل المسارات
-    app.register_blueprint(auth_bp, url_prefix='/sing')
-    app.register_blueprint(users_bp, url_prefix='/profile_me')
+    app.register_blueprint(auth_bp, url_prefix='/sign')
+    app.register_blueprint(users_bp, url_prefix='/api/users')
     app.register_blueprint(home_bp, url_prefix='/')
+    app.register_blueprint(posts_bp, url_prefix='/api/posts')  # إضافة مسار المنشورات
 
     # ✅ إنشاء قاعدة البيانات إذا لم تكن موجودة
     with app.app_context():
@@ -60,9 +59,4 @@ def webhook():
         os.system("cd /home/moon2013/moon && git push origin main")  # دفع التعديلات إلى GitHub
 
         return "Repository updated, server restarted, and changes pushed to GitHub!", 200
-    return "Invalid request", 400  
-  
-  
-  
-  
-  
+    return "Invalid request", 400
